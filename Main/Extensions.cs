2375b5b32fc72ff01f4a7e2758d4c28b171402a3
Miron Sorin <smiron86@gmail.com>
2012-12-03 23:18:50 +0200

    Added array extensions

---
+++ Main/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>(this T[] elements, Action<T> action)
+        {
+            foreach (var element in elements)
+            {
+                action(element);
+            }
+        }
+
+        public static void ForEach<T>(this T[,] elementsMatrix, Action<T> action)
+        {
+            foreach (var elementsRow in elementsMatrix)
+            {
+                action(elementsRow);
+            }
+        }
+    }
+}
